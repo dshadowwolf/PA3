8b29fe84d96c6f2efbeec583f82646489b479641
Daniel Hazelton <dshadowwolf@gmail.com>
2020-08-17 17:34:17 -0400

    Lots of minor changes to get redstone output of tanks to work and the start of major work on making pipes function. A lot more work is going to be needed for the pipes, I think...

---
+++ src/main/java/com/mcmoddev/poweradvantage/tiles/TileFluidConduit.java
@@ -0,0 +1,19 @@
+package com.mcmoddev.poweradvantage.tiles;
+
+import com.mcmoddev.lib.container.gui.GuiContext;
+import com.mcmoddev.lib.container.gui.IWidgetGui;
+import com.mcmoddev.lib.tile.MMDStandardTileEntity;
+
+public class TileFluidConduit extends MMDStandardTileEntity {
+
+	public TileFluidConduit() {
+		
+	}
+	
+	@Override
+	protected IWidgetGui getMainContentWidgetGui(GuiContext context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
